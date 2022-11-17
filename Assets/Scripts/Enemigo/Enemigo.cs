d40fb0abd17d336c067dfe7ed36fd25af494450b
DanielMayoral <d.mayoral.2019@alumnos.urjc.es>
2022-11-17 21:18:28 +0100

    Cambio enemigos
    La creación se ha pasado de EnemigoController a Enemigos

--- Assets/Scripts/Enemigo/Enemigo.cs
+++ Assets/Scripts/Enemigo/Enemigo.cs
@@ -6,6 +6,7 @@ public class Enemigo
     [SerializeField]
     private float vida;
 
+    [SerializeField]
     private float vidaMax;
 
     [SerializeField]
@@ -18,7 +19,51 @@ public class Enemigo
     [SerializeField]
     private TipoAtaque tipoAtaque;
 
-    public Enemigo() { }
+    public Enemigo(TipoEnemigo tipoEnemigo) 
+    {
+        switch (tipoEnemigo)
+        {
+            case TipoEnemigo.PERRO_INFERNAL:
+                ataque = Random.Range(2, 7);
+                defensa = Random.Range(1, 6);
+                vida = Random.Range(5, 15);
+                vidaMax = vida;
+                tipoAtaque = (TipoAtaque)Random.Range(0, 2);
+                break;
+
+            case TipoEnemigo.MURICELAGO_VAMPIRO:
+                ataque = Random.Range(3, 9);
+                defensa = Random.Range(2, 6);
+                vida = Random.Range(5, 20);
+                vidaMax = vida;
+                tipoAtaque = (TipoAtaque)Random.Range(1, 3);
+                break;
+
+            case TipoEnemigo.GOBLIN:
+                ataque = Random.Range(5, 10);
+                defensa = Random.Range(4, 8);
+                vida = Random.Range(10, 25);
+                vidaMax = vida;
+                tipoAtaque = (TipoAtaque)Random.Range(0, 3);
+                break;
+
+            case TipoEnemigo.DEMONIO_HEMBRA:
+                ataque = Random.Range(8, 20);
+                defensa = Random.Range(8, 15);
+                vida = Random.Range(15, 30);
+                vidaMax = vida;
+                tipoAtaque = (TipoAtaque)Random.Range(0, 5);
+                break;
+
+            case TipoEnemigo.DEMONIO_MACHO:
+                ataque = Random.Range(8, 20);
+                defensa = Random.Range(8, 15);
+                vida = Random.Range(15, 30);
+                vidaMax = vida;
+                tipoAtaque = (TipoAtaque)Random.Range(0, 5);
+                break;
+        }
+    }
 
     public float GetVida() { return this.vida; }
     public float GetVidaMax() { return this.vidaMax; }
